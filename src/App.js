c7efc768548a2ea21b5b097bdd5895c512c51c97
Andrea Bruno <andreabrunoxy@gmail.com>
2021-10-01 11:13:24 +0200

    Add Modal & Code Optimization

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
-import React, { useState } from "react";
-import logo from "./images/logo.png";
+import React, { useState, useEffect } from 'react';
+import { questions } from './data/data';
+import logo from './assets/images/logo.png';
+import click from './assets/sounds/click.mp3';
 
 export default function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
+  const [modalIsOpen, setModalIsOpen] = useState(true);
 
-  const questions = [
-    {
-      questionText: "Il bicchiere lo vedi...",
-      answerOptions: [
-        { answerText: "Mezzo pieno.", points: 5 },
-        { answerText: "Mezzo vuoto.", points: 3 },
-        { answerText: "Chi si è fregato il bicchiere?", points: 1 }
-      ]
-    },
-    {
-      questionText: "Come usciremo dalla pandemia?",
-      answerOptions: [
-        { answerText: "Migliori.", points: 5 },
-        { answerText: "Peggiori.", points: 3 },
-        { answerText: "Moriremo tutti!", points: 1 }
-      ]
-    },
-    {
-      questionText: "Ti fidi dei vaccini?",
-      answerOptions: [
-        { answerText: "Pienamente.", points: 5 },
-        { answerText: "Beh, se non c'è alternativa...", points: 3 },
-        { answerText: "Moriremo tutti!", points: 1 }
-      ]
-    },
-    {
-      questionText: "Ti fidi delle persone?",
-      answerOptions: [
-        { answerText: "Direi di si.", points: 5 },
-        { answerText: "La fregatura è dietro l'angolo.", points: 3 },
-        { answerText: "Devono morire tutti!", points: 1 }
-      ]
-    }
-  ];
+  useEffect(() => {
+    setModalIsOpen(true);
+  }, []);
 
   function handleAnswerButtonClick(points) {
+    new Audio(click).play();
     if (points) {
       setScore(score + points);
     }
@@ -54,98 +27,78 @@ export default function App() {
   }
 
   function handleReset() {
+    new Audio(click).play();
     setCurrentQuestion(0);
     setScore(0);
     setShowScore(false);
   }
 
   //RENDERING JSX DELL'APP//
-  if (showScore && score > 16) {
-    return (
-      <>
-        <h1>Clamoroso Test</h1>
-        <h2>Sei ottimista o pessimista?</h2>
-        <div className="app">
-          <div className="score-section">
-            Il tuo punteggio è di: <br /> {score} su {questions.length * 5}. <br />
-            <br />
-            Congratulazioni!
-            <br /> Sei un ottimista!
-          </div>
-          <button className="button-reset" onClick={() => handleReset()}>
-            Ricomincia
-          </button>
-          <div className="image">
-            <img src={logo} alt="logo" />
-          </div>
-        </div>
-      </>
-    );
-  } else if (showScore && score >= 6 && score <= 16) {
-    return (
-      <>
-        <h1>Clamoroso Test</h1>
-        <h2>Sei ottimista o pessimista?</h2>
-        <div className="app">
-          <div className="score-section">
-            Il tuo punteggio è di: <br />
-            {score} su {questions.length * 5}. <br />
-            <br />
-            Beh dai! <br /> Sei una persona realista...
-          </div>
-          <button className="button-reset" onClick={() => handleReset()}>
-            Ricomincia
-          </button>
-          <div className="image">
-            <img src={logo} alt="logo" />
-          </div>
-        </div>
-      </>
-    );
-  } else if (showScore && score < 6) {
-    return (
-      <>
-        <h1>Clamoroso Test</h1>
-        <h2>Sei ottimista o pessimista?</h2>
-        <div className="app">
-          <div className="score-section">
-            Il tuo punteggio è di: <br /> {score} su {questions.length * 5}. <br />
-            <br />
-            Meglio che tu non sappia il tuo profilo...
-          </div>
-          <button className="button-reset" onClick={() => handleReset()}>
-            Ricomincia
-          </button>
-          <div className="image">
-            <img src={logo} alt="logo" />
-          </div>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h1>Clamoroso Test</h1>
-        <h2>Sei ottimista o pessimista?</h2>
-        <div className="app">
-          <div className="question-section">
-            <div className="question-count">
-              <span>Domanda {currentQuestion + 1}</span>/{questions.length}
+  return (
+    <>
+      {modalIsOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h1>TEST</h1>
+            <h2>Sei pessimista o ottimista?</h2>
+            <p>Rispondi a tutte le domande e alla fine scoprirai il risultato.</p>
+            <p>Buon divertimento!</p>
+            <button className="button-modal" onClick={() => setModalIsOpen(false)}>
+              Inizia il test!
+            </button>
+            <div className="image">
+              <img src={logo} alt="logo" />
             </div>
-            <div className="question-text">{questions[currentQuestion].questionText}</div>
           </div>
-          <div className="answer-section">
-            {questions[currentQuestion].answerOptions.map((answerOption, index) => (
-              <button className="button-answer" onClick={() => handleAnswerButtonClick(answerOption.points)}>
-                {answerOption.answerText}
+        </div>
+      )}
+      <h1>TEST</h1>
+      <h2>Sei pessimista o ottimista?</h2>
+
+      <div className="app-container">
+        <div className="app">
+          {showScore && (
+            <>
+              <div className="score-section">
+                Il tuo punteggio è: <br /> {score} su {questions.length * 5}. <br />
+                <br />
+                {score > 16 ? 'Congratulazioni! Sei un ottimista!' : ''}
+                {score >= 6 && score <= 16 ? 'Beh dai! Sei una persona realista!' : ''}
+                {score < 6 ? 'Lasciamo stare...meglio non sapere!' : ''}
+              </div>
+              <button className="button-reset" onClick={() => handleReset()}>
+                Ricomincia
               </button>
-            ))}
-          </div>
+            </>
+          )}
+          {!showScore && (
+            <>
+              <div className="question-section">
+                <div className="question-count">
+                  <span>Domanda {currentQuestion + 1}</span>/{questions.length}
+                </div>
+                <div className="question-text">
+                  {questions[currentQuestion].questionText}
+                </div>
+              </div>
+              <div className="answer-section">
+                {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+                  <button
+                    className="button-answer"
+                    onClick={() => handleAnswerButtonClick(answerOption.points)}
+                  >
+                    {answerOption.answerText}
+                  </button>
+                ))}
+              </div>
+            </>
+          )}
           <div className="image">
             <img src={logo} alt="logo" />
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+      <footer>Made with ❤️ by Andrea Bruno</footer>
+    </>
+  );
 }
---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+export const questions = [
+  {
+    questionText: 'Il bicchiere lo vedi...',
+    answerOptions: [
+      { answerText: 'Mezzo pieno.', points: 5 },
+      { answerText: 'Mezzo vuoto.', points: 3 },
+      { answerText: 'Chi si è fregato il bicchiere?', points: 1 }
+    ]
+  },
+  {
+    questionText: 'Come usciremo dalla pandemia?',
+    answerOptions: [
+      { answerText: 'Migliori.', points: 5 },
+      { answerText: 'Peggiori.', points: 3 },
+      { answerText: 'Moriremo tutti!', points: 1 }
+    ]
+  },
+  {
+    questionText: 'Ti fidi dei vaccini?',
+    answerOptions: [
+      { answerText: 'Pienamente.', points: 5 },
+      { answerText: "Beh, se non c'è alternativa...", points: 3 },
+      { answerText: 'Moriremo tutti!', points: 1 }
+    ]
+  },
+  {
+    questionText: 'Ti fidi delle persone?',
+    answerOptions: [
+      { answerText: 'Direi di si.', points: 5 },
+      { answerText: "La fregatura è dietro l'angolo.", points: 3 },
+      { answerText: 'Devono morire tutti!', points: 1 }
+    ]
+  }
+];
